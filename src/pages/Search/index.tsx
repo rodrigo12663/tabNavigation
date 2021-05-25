20b5c5d7adb4671c87fd093007529f699b37644f
Rodrigo <rodrigovieiranunes23@outlook.com>
2021-05-25 08:40:51 -0300

    add pages navigation

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Bem vindo a tab navigation</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
